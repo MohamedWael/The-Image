606907f99b2b9ed76a59e8b85f24b2c909f2c39d
Mohamed Wael <mohamed.wael@v-apps.co>
2019-02-22 20:52:12 +0200

    adding photo photo response

---
+++ app/src/main/java/com/blogspot/mowael/mvvmsample/modules/home/response/Links.kt
@@ -0,0 +1,8 @@
+package com.blogspot.mowael.mvvmsample.modules.home.response
+
+data class Links(
+    val download: String,
+    val download_location: String,
+    val html: String,
+    val self: String
+)
